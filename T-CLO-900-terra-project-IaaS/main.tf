12346d5ed3a69947edcd79603089358153ea28f9
unknown <vladislav.razyvika@gmail.com>
2024-12-12 16:12:04 +0100

    Iaas inc

--- T-CLO-900-terra-project-IaaS/main.tf
+++ T-CLO-900-terra-project-IaaS/main.tf
@@ -1,18 +1,18 @@
-data "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "t_clo" {
   name = var.resource_group_name
 }
  
 # Récupérer le DevTest Lab existant
-data "azurerm_dev_test_lab" "example" {
+data "azurerm_dev_test_lab" "t_clo" {
   name                = var.devtestlab_name
-  resource_group_name = data.azurerm_resource_group.example.name
+  resource_group_name = data.azurerm_resource_group.t_clo.name
 }
 
 
 resource "azurerm_dev_test_linux_virtual_machine" "vmapp" {
   name                   = "Linux-app"
-  lab_name               = data.azurerm_dev_test_lab.example.name
-  resource_group_name    = data.azurerm_resource_group.example.name
+  lab_name               = data.azurerm_dev_test_lab.t_clo.name
+  resource_group_name    = data.azurerm_resource_group.t_clo.name
   location               = var.location
   size                   = "Standard_A4_v2"    
   username               = var.username-app
@@ -29,26 +29,6 @@ resource "azurerm_dev_test_linux_virtual_machine" "vmapp" {
   }
 }
 
-  resource "azurerm_dev_test_linux_virtual_machine" "vmsql" {
-  name                   = "Linux-sql"
-  lab_name               = data.azurerm_dev_test_lab.example.name
-  resource_group_name    = data.azurerm_resource_group.example.name
-  location               = var.location
-  size                   = "Standard_A4_v2"    
-  username               = var.username-sql
-  password               = var.password-sql   
-  lab_virtual_network_id = var.lab_virtual_network_id
-  lab_subnet_name        = var.lab_subnet_name
-  storage_type           = "Standard"      
-
-  gallery_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-}
 
 
 
