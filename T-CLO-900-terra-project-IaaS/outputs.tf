767d7202b2bd2104de8a50a4bf278a4c9746f1da
unknown <vladislav.razyvika@gmail.com>
2024-12-13 01:22:22 +0100

    wip
    Signed-off-by: unknown <vladislav.razyvika@gmail.com>

---
+++ T-CLO-900-terra-project-IaaS/outputs.tf
@@ -0,0 +1,20 @@
+output "lab_name" {
+  description = "Dev test lab name"
+  value       = var.devtestlab_name
+}
+
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = var.resource_group_name
+}
+
+
+output "vm_name" {
+  value = azurerm_dev_test_linux_virtual_machine.vmapp.name
+}
+
+output "vm_fqdn" {
+  value = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
+}
+
+  
