adaf71c20650705faa6895975cd3ffc9ae8017ba
unknown <vladislav.razyvika@gmail.com>
2024-12-17 21:43:20 +0100

    update: feature done
    Signed-off-by: unknown <vladislav.razyvika@gmail.com>

--- T-CLO-900-terra-project-IaaS/main.tf
+++ T-CLO-900-terra-project-IaaS/main.tf
@@ -37,41 +37,21 @@ resource "azurerm_dev_test_linux_virtual_machine" "vmapp" {
   }
 }
 
-# # Install Python and Ansible
+# Install Python and Ansible
 resource "null_resource" "setup_ansible" {
-    provisioner "remote-exec" {
-       inline = [
-      # "set -eux",
-
-      # # Désactiver complètement les prompts interactifs pour APT et dpkg
+  provisioner "remote-exec" {
+    inline = [
+      # Installation et vérification (décommenter les parties nécessaires)
       # "export DEBIAN_FRONTEND=noninteractive",
-      # "echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections",
-
-      # # Attente pour libérer le verrou dpkg
-      # "while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do echo 'Waiting for dpkg lock release...'; sleep 2; done",
-
-      # # Installer needrestart et configurer le redémarrage automatique silencieux
-      # "sudo apt-get update -y",
-      # "sudo apt-get install -y needrestart",
-      # "sudo sed -i 's/^#\\$nrconf{restart} = .*/\\$nrconf{restart} = \"a\";/' /etc/needrestart/needrestart.conf",
-
-      # # Configuration temporaire pour apt pour éviter les redémarrages interactifs
-      # "sudo mkdir -p /etc/apt/apt.conf.d",
-      # "echo 'DPkg::Options { \"--force-confdef\"; \"--force-confold\"; }' | sudo tee /etc/apt/apt.conf.d/local",
-
-      # # Installation silencieuse des paquets nécessaires
-      # "sudo apt-get upgrade -y",
+      # "sudo apt-get update -y && sudo apt-get upgrade -y",
       # "sudo apt-get install -y python3 python3-pip sshpass",
-
-      # # Installation d'Ansible via pip3
       # "pip3 install --upgrade pip",
       # "pip3 install --user ansible",
-
-      # Vérification des installations
       "python3 --version",
       "pip3 --version",
       "~/.local/bin/ansible --version"
     ]
+
     connection {
       type        = "ssh"
       host        = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
@@ -81,36 +61,6 @@ resource "null_resource" "setup_ansible" {
   }
 
   depends_on = [azurerm_dev_test_linux_virtual_machine.vmapp]
-}
-
-resource "null_resource" "upload_ssh_key" {
-  provisioner "file" {
-    source      = "./ssh/id_ed25519"
-    destination = "/home/${var.username_app}/.ssh/id_ed25519"
-
-    connection {
-      type        = "ssh"
-      host        = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
-      user        = var.username_app
-      private_key = file("./ssh/id_ed25519")
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      # Configurer les permissions de la clé privée
-      "chmod 600 /home/${var.username_app}/.ssh/id_ed25519",
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
-      user        = var.username_app
-      private_key = file("./ssh/id_ed25519")
-    }
-  }
-
-  depends_on = [azurerm_dev_test_linux_virtual_machine.vmapp, null_resource.upload_ansible]
 }
 
 # Upload Ansible directory
@@ -129,22 +79,24 @@ resource "null_resource" "upload_ansible" {
 
   depends_on = [null_resource.setup_ansible]
 }
+# Upload SSH key (private)
+resource "null_resource" "upload_ssh_key" {
+  provisioner "file" {
+    source      = "./ssh/id_ed25519"
+    destination = "/home/${var.username_app}/.ssh/id_ed25519"
 
-resource "null_resource" "generate_inventory" {
+    connection {
+      type        = "ssh"
+      host        = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
+      user        = var.username_app
+      private_key = file("./ssh/id_ed25519")
+    }
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "cd /home/${var.username_app}/ansible/inventories",
-      # Vérifie si l'adresse de l'host existe déjà dans le fichier
-      "if ! grep -q \"ansible_host: ${azurerm_dev_test_linux_virtual_machine.vmapp.fqdn}\" hosts.yml; then",
-      "  echo all: > hosts.yml",
-      "  echo \"  hosts:\" >> hosts.yml",
-      "  echo \"    slave1:\" >> hosts.yml",
-      "  echo \"      ansible_host: ${azurerm_dev_test_linux_virtual_machine.vmapp.fqdn}\" >> hosts.yml",
-      "  echo \"      ansible_user: ${var.username_app}\" >> hosts.yml",
-      "  echo \"      ansible_password: ${var.password_app}\" >> hosts.yml",
-      "else",
-      "  echo 'Host with address ${azurerm_dev_test_linux_virtual_machine.vmapp.fqdn} already exists in hosts.yml. Skipping.'",
-      "fi"
+      "chmod 700 /home/${var.username_app}/.ssh",
+      "chmod 600 /home/${var.username_app}/.ssh/id_ed25519"
     ]
 
     connection {
@@ -155,18 +107,15 @@ resource "null_resource" "generate_inventory" {
     }
   }
 
-  depends_on = [null_resource.upload_ansible]
+  depends_on = [azurerm_dev_test_linux_virtual_machine.vmapp, null_resource.upload_ansible]
 }
 
-
-# Run Ansible playbook
-resource "null_resource" "run_playbook" {
+# Convert the key to Unix format using dos2unix
+resource "null_resource" "format_ssh_key" {
   provisioner "remote-exec" {
     inline = [
-      "set -eux",
-            # Ajouter la clé de slave1 dans ~/.ssh/known_hosts
-      "mkdir -p ~/.ssh && ssh-keyscan -H ${azurerm_dev_test_linux_virtual_machine.vmapp.fqdn} >> ~/.ssh/known_hosts",
-     "~/.local/bin/ansible-playbook -i /home/${var.username_app}/ansible/inventories/hosts.yml --private-key /home/${var.username_app}/.ssh/id_ed25519 /home/${var.username_app}/ansible/playbook.yml -vv"
+      "sudo apt-get update && sudo apt-get install -y dos2unix",
+      "dos2unix /home/${var.username_app}/.ssh/id_ed25519"
     ]
 
     connection {
@@ -178,4 +127,50 @@ resource "null_resource" "run_playbook" {
   }
 
   depends_on = [null_resource.upload_ssh_key]
+}
+
+# Generate inventory (unchanged)
+resource "null_resource" "generate_inventory" {
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/${var.username_app}/ansible/inventories",
+      "cd /home/${var.username_app}/ansible/inventories",
+      "echo 'all:' > hosts.yml",
+      "echo '  hosts:' >> hosts.yml",
+      "echo '    slave1:' >> hosts.yml",
+      "echo '      ansible_host: ${azurerm_dev_test_linux_virtual_machine.vmapp.fqdn}' >> hosts.yml",
+      "echo '      ansible_user: ${var.username_app}' >> hosts.yml",
+      "echo '      ansible_ssh_private_key_file: /home/${var.username_app}/.ssh/id_ed25519' >> hosts.yml",
+      "echo '      ansible_ssh_common_args: \"-o StrictHostKeyChecking=no\"' >> hosts.yml"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
+      user        = var.username_app
+      private_key = file("./ssh/id_ed25519")
+    }
+  }
+
+  depends_on = [null_resource.upload_ansible, null_resource.format_ssh_key]
+}
+
+# Run Ansible playbook
+resource "null_resource" "run_playbook" {
+  provisioner "remote-exec" {
+    inline = [
+      "set -eux",
+      "mkdir -p ~/.ssh && ssh-keyscan -H ${azurerm_dev_test_linux_virtual_machine.vmapp.fqdn} >> ~/.ssh/known_hosts",
+      "~/.local/bin/ansible-playbook -i /home/${var.username_app}/ansible/inventories/hosts.yml /home/${var.username_app}/ansible/playbook.yml -vv"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = azurerm_dev_test_linux_virtual_machine.vmapp.fqdn
+      user        = var.username_app
+      private_key = file("./ssh/id_ed25519")
+    }
+  }
+
+  depends_on = [null_resource.generate_inventory]
 }
